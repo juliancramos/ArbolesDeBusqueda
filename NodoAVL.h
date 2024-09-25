98044ef8e635b30b077ffa15153725721c614c3a
Julian Ramos <julianc.ramos21@gmail.com>
2024-09-25 15:18:56 -0500

    Se agrega el nodo AVL

---
+++ NodoAVL.h
@@ -0,0 +1,43 @@
+#ifndef _NODO_AVL_H_
+#define _NODO_AVL_H_
+
+#include <string>
+
+template <class T>
+class NodoAVL{
+    protected:
+        T dato;
+        NodoAVL <T>* hijoIzq;
+        NodoAVL <T>* hijoDer;
+    
+    public: 
+        NodoAVL();
+        ~NodoAVL();
+        T& getDato();
+        NodoAVL<T>* getHijoIzq() const;
+        NodoAVL<T>* getHijoDer() const;
+        bool esHoja();
+        void setDato(T& datoNuevo);
+        int altura();
+        void setHijoIzq(NodoAVL<T>* izq);
+        void setHijoDer(NodoAVL<T>* der);
+        void preOrden();
+        void posOrden();
+        void inOrden();
+        void nivelOrden();
+        int tam();
+
+        //Metodos de balanceo
+        int factorBalance();
+        NodoAVL<T>* balancearNodo();
+        //Rotaciones
+        NodoAVL<T>* rotacionDerecha();
+        NodoAVL<T>* rotacionIzquierda();
+        NodoAVL<T>* rotacionDerechaIzquierda();
+        NodoAVL<T>* rotacionIzquierdaDerecha();
+        
+};
+
+#include "NodoAVL.hxx"
+
+#endif
